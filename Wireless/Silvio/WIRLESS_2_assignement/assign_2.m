2a7b919529261f01943a6a7bdab4ccfca12b3a9d
Silvio Zanoli <zanolisilvio@yahoo.it>
2017-10-03 22:56:40 +0200

    synced!

--- Wireless/Silvio/WIRLESS_2_assignement/assign_2.m
+++ Wireless/Silvio/WIRLESS_2_assignement/assign_2.m
@@ -1,6 +1,15 @@
 clear
 sign=load('task2.mat');
-SNRdb=1:2:10;
+SNRdb=100;
 sigH=sqrt((10^(-SNRdb/10))/2);
 noise=randn(1,length(sign.signal))*sigH+1i*randn(1,length(sign.signal))*sigH;
 received=sign.signal+noise.';
+pattern=(LFSR);
+convVect=zeros(length(pattern),1);
+convVal=zeros(length(sign.signal),1);
+for i=1:(length(sign.signal)-length(pattern))
+    convVect(:,1)=received(i:i+length(pattern)-1,1);
+    convVal(i)=abs(pattern'*convVect);
+end
+
+plot(convVal)
