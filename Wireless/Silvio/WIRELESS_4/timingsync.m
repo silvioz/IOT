d80e6cceedba46be7ccc9391c9b8f81b69e374fb
Silvio Zanoli <zanolisilvio@yahoo.it>
2017-10-21 23:24:50 +0200

    end ass4

--- Wireless/Silvio/WIRELESS_4/timingsync.m
+++ Wireless/Silvio/WIRELESS_4/timingsync.m
@@ -1,8 +1,9 @@
+clear
 
 os_factor = 4;
 SNR = 10;
 
-load ./task4
+load task4
 
 data_length = prod(image_size) * 8 / 2; % Number of QPSK data symbols
 
@@ -23,21 +24,25 @@ data = zeros(1,data_length);
 cum_err = 0;
 diff_err = zeros(1,data_length);
 epsilon  = zeros(1,data_length);
+exp_vect=[1 ; -i ; -1 ; i];
 
 for i=1:data_length
     
      idx_start  = data_idx+(i-1)*os_factor;
      idx_range  = idx_start:idx_start+os_factor-1;
      segment    = filtered_rx_signal(idx_range);
-    
-     % Estimate timing error epsilon
-
-     % Chose best sampling point
+     diff_err(i)= (abs(segment).^2).'*exp_vect;
      
-     % Interpolate linear
+     cum_err=cum_err+diff_err(i);
+     epsilon(i)=(-1/(2*pi)).*angle(cum_err);
+     
+     start_interpolator_indx=idx_start+floor(4*epsilon(i));
+     x=abs((4*epsilon(i))-floor(4*epsilon(i)));
 
-     data(i) = ...;
+     data(i) = (1-x)*filtered_rx_signal(start_interpolator_indx)+x*filtered_rx_signal(start_interpolator_indx+1);
      
 end
 
 image_decoder(demapper(data), image_size);
+figure;
+plot(epsilon);
